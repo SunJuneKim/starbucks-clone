debb3692ba7d5864a48d7c623038b464d0acf02b
KSJ <fly881004@gamil.com>
2022-11-10 19:40:32 +0900

    hamburger nav 만드는중

---
+++ script.js
@@ -0,0 +1,9 @@
+const hamburger = document.getElementsByClassName('hamburger-wrap');
+const clicked = document.querySelectorAll('input').clicked;
+// const nav-menu = document.getElementsByClassName('hamburger-nav');
+const a =document.getElementsByClassName('a');
+
+if(clicked){
+    a.className+='hamburger-nav';
+    console.log(first);
+}
